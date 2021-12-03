8bf3c89ad05b2d0a0646dd988719795a7a6e62fc
cccyuxuan <1003347975@qq.com>
2021-12-03 21:40:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lbs/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.lbs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView=findViewById(R.id.bmapView);
+        mapView.getMap().setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+        mBaiduMap = mapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        //mBaiduMap.setIndoorEnable(true);
+        init();
+    }
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+        }
+    }
+
+    public void init(){
+        //定位初始化
+        mLocationClient = new LocationClient(this);
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        //注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        //开启地图定位图层
+        mLocationClient.start();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        //mapView.onDestroy();
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mapView.onDestroy();
+        mapView = null;
+        super.onDestroy();
+    }
+}
